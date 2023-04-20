7769f18be64fa76c47bf2755c3f408ebc13caed7
Felicity penny <felicitypenny3@gmail.com>
2023-04-20 17:55:36 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/index.js
+++ options/index.js
@@ -1,27 +1,26 @@
-import React from "react"
-import * as modsOpts from "glob:../modules/**/options.js"
-import { modulesOptions, globalOptions } from "./options"
+import React from "react";
+import * as modsOpts from "glob:../modules/**/options.js";
+import { modulesOptions, globalOptions } from "./options";
 
 const parseOpts = opts => {
-  let temp = {}
+  let temp = {};
+
   for (const [name, definition] of Object.entries(opts)) {
-    let key = `${name.charAt(0).toUpperCase()}${name.slice(1)}`
-    temp[key] = definition
+    let key = `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+    temp[key] = definition;
   }
-  return temp
-}
 
-const options = parseOpts(modsOpts)
+  return temp;
+};
 
+const options = parseOpts(modsOpts);
 export function getOptions(component) {
-  let target = options[component.name] || {}
-  let source = modulesOptions[component.name] || {}
-  return Object.assign(target, source)
+  let target = options[component.name] || {};
+  let source = modulesOptions[component.name] || {};
+  return Object.assign(target, source);
 }
-
 export function getGlobalOptions() {
-  return globalOptions
+  return globalOptions;
 }
-
-export const OptionsContext = React.createContext(options)
-export const GlobalOptionsContext = React.createContext(globalOptions)
+export const OptionsContext = React.createContext(options);
+export const GlobalOptionsContext = React.createContext(globalOptions);
--- options/options.js
+++ options/options.js
@@ -1,6 +1,7 @@
 export const globalOptions = {
-  name: "pen1233_29563Identifier",
-  url: "https://pen1233_29563Identifier.botics.co",
-  api: "https://pen1233_29563Identifier.botics.co/api/v1"
-}
-export const modulesOptions = {}
+  "name": "pen1233_29563Identifier",
+  "url": "https://pen1233_29563Identifier.botics.co",
+  "api": "https://pen1233_29563Identifier.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
+export const modulesOptions = {};
